8984d97e618d74e7c8e5b6ca202b0cc326d391cc
Ernando Rezende <ernando.03@gmail.com>
2025-03-28 23:54:58 +0000

    Created the method to print the board

--- src/main/java/br/com/devcompleto/TicTacToe.java
+++ src/main/java/br/com/devcompleto/TicTacToe.java
@@ -1,9 +1,25 @@
 package br.com.devcompleto;
 
 public class TicTacToe {
-    public static void main(String[] args)
-    {
+    static String [] board = {"1", "2" ,"3", "4", "5", "6", "7", "8", "9" };
 
-        System.out.println("TIC TAC TOE");
+    public static void main(String[] args) {
+
+       printBoard();
+
+
+
+    }
+
+    private static  void printBoard() {
+        System.out.println("\n\tTIC TAC TOE GAME");
+        System.out.println("========================");
+        System.out.print("\t     |     |\n");
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[0], board[1], board[2]);
+        System.out.printf("\t--------------------\n");
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[3], board[4], board[5]);
+        System.out.printf("\t--------------------\n");
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[6], board[7], board[8]);
+        System.out.print("\t     |     |\n");
     }
 }
