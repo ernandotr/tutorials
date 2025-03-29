ad1026f904af00f76d092b951e5772cf909763fe
Ernando Rezende <ernando.03@gmail.com>
2025-03-29 22:01:43 +0000

    Improved  age calc to read a birth date by default inpunt

--- src/main/java/br/com/devcompleto/AgeCalc.java
+++ src/main/java/br/com/devcompleto/AgeCalc.java
@@ -1,15 +1,20 @@
 package br.com.devcompleto;
 
+import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class AgeCalc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter a birth date to calc the age: ");
+        String birthDate =  sc.nextLine();
+        LocalDate birth =  LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        LocalDate birth = LocalDate.of(1979, 6, 29);
+//        LocalDate birth = LocalDate.of(1979, 6, 29);
         LocalDate today = LocalDate.now();
         Period period = Period.between(birth, today);
         System.out.println(period.getYears());
