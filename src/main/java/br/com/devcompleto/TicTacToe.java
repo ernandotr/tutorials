372df47b79b1bfcf84bf8ba01cfefe3e3c6d471c
Ernando Rezende <ernando.03@gmail.com>
2025-03-29 15:46:58 +0000

    Adjusted the board

--- src/main/java/br/com/devcompleto/TicTacToe.java
+++ src/main/java/br/com/devcompleto/TicTacToe.java
@@ -1,25 +1,26 @@
 package br.com.devcompleto;
 
 public class TicTacToe {
-    static String [] board = {"1", "2" ,"3", "4", "5", "6", "7", "8", "9" };
+    static String [] board = {"", "1", "2" ,"3", "4", "5", "6", "7", "8", "9" };
 
     public static void main(String[] args) {
 
-       printBoard();
+       drawBoard();
 
 
 
     }
 
-    private static  void printBoard() {
+    private static  void drawBoard() {
         System.out.println("\n\tTIC TAC TOE GAME");
         System.out.println("========================");
         System.out.print("\t     |     |\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[0], board[1], board[2]);
-        System.out.printf("\t--------------------\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[3], board[4], board[5]);
-        System.out.printf("\t--------------------\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[6], board[7], board[8]);
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[1], board[2], board[3]);
+        System.out.print("\t--------------------\n");
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[4], board[5], board[6]);
+        System.out.print("\t--------------------\n");
+        System.out.printf("\t  %s  |  %s  |  %s  \n", board[7], board[8], board[9]);
         System.out.print("\t     |     |\n");
+        System.out.println("========================");
     }
 }
