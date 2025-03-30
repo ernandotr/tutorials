1699eeb18995d2e866b9306856fbedbe166c3bd0
Ernando Rezende <ernando.03@gmail.com>
2025-03-31 00:02:05 +0100

    Extract clearScrean feature to a new method

--- src/main/java/br/com/devcompleto/TicTacToe.java
+++ src/main/java/br/com/devcompleto/TicTacToe.java
@@ -4,24 +4,28 @@ public class TicTacToe {
     static String [] board = {"", "1", "2" ,"3", "4", "5", "6", "7", "8", "9" };
 
     public static void main(String[] args) {
-
-       drawBoard();
+        clearScreen();
+        drawBoard();
 
 
 
     }
 
     private static  void drawBoard() {
-        System.out.print("\033[H\033[2J");
+
         System.out.println("\n\tTIC TAC TOE GAME");
-        System.out.println("========================");
-        System.out.print("\t     |     |\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[1], board[2], board[3]);
-        System.out.print("\t--------------------\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[4], board[5], board[6]);
-        System.out.print("\t--------------------\n");
-        System.out.printf("\t  %s  |  %s  |  %s  \n", board[7], board[8], board[9]);
-        System.out.print("\t     |     |\n");
-        System.out.println("========================");
+        System.out.println("  ========================");
+        System.out.print("         |     |\n\r");
+        System.out.printf("      %s  |  %s  |  %s  \n\r", board[1], board[2], board[3]);
+        System.out.print("    --------------------\n");
+        System.out.printf("      %s  |  %s  |  %s  \n", board[4], board[5], board[6]);
+        System.out.print("    --------------------\n");
+        System.out.printf("      %s  |  %s  |  %s  \n", board[7], board[8], board[9]);
+        System.out.print("         |     |\n\r");
+        System.out.println("  ========================");
+    }
+
+    private static void clearScreen() {
+        System.out.print("\033[H\033[2J");
     }
 }
