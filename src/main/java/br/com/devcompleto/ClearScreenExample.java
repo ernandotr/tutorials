4070ec34acb6173b789179be431f98db22d1a088
Ernando Rezende <ernando.03@gmail.com>
2025-03-30 23:46:47 +0100

    Created class to test clear screen

---
+++ src/main/java/br/com/devcompleto/ClearScreenExample.java
@@ -0,0 +1,17 @@
+package br.com.devcompleto;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class ClearScreenExample {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+        System.out.println("================= CLS ================");
+        Scanner scanner = new Scanner(System.in);
+        String read =  scanner.nextLine();
+        scanner.close();
+//        System.out.print("\033c");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
