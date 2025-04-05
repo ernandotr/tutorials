72a51b7adfe21153fac36a5e03fdb3b616259c48
Ernando Rezende <ernando.03@gmail.com>
2025-04-05 23:50:28 +0100

    Printing emojis on animated text

--- src/main/java/br/com/devcompleto/Ticker.java
+++ src/main/java/br/com/devcompleto/Ticker.java
@@ -2,23 +2,29 @@ package br.com.devcompleto;
 
 public class Ticker {
     public static void main(String... args) throws InterruptedException {
-        String str = "<| Welcome to java text mode animation! |> ";
+        String str = "\uD83D\uDEB5\uD83C\uDFFB\u200D♀️Welcome to \uD83D\uDC7D java text mode animation!\uD83D\uDEA3\uD83C\uDFFB\u200D♂️";
         int width = 60;
         int spaces = width - str.length();
-        String line="";
 
-        // Replace ⋖ by the actual angle brackets as YT doesn't allow angle brackets in description.
-        for(int i = 1; i <= spaces; i++) {
-            line+=" ";
-        }
+        StringBuilder line= new StringBuilder();
+        line.append(" ".repeat(spaces));
+        line.append(str);
 
-        line += str;
-        System.out.print("\033[H\033[2J");
-        System.out.println("\u001b[35;1m");
+        clearScreen();
+        changeTextColor();
+
         while(true){
             System.out.print("\r"+line);
             Thread.sleep(500);
-            line = line.substring(1)+line.charAt(0);
+            line = new StringBuilder(line.substring(1) + line.charAt(0));
         }
+    }
+
+    private static void changeTextColor() {
+        System.out.println("\u001b[35;1m");
+    }
+
+    private static void clearScreen() {
+        System.out.print("\033[H\033[2J");
     }
 }
