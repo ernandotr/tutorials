43a362770c03b1a233336950edb18e42772477b3
Ernando Rezende <ernando.03@gmail.com>
2025-04-05 20:58:38 +0100

    Improving board view

--- src/main/java/br/com/devcompleto/TicTacToe.java
+++ src/main/java/br/com/devcompleto/TicTacToe.java
@@ -8,6 +8,9 @@ public class TicTacToe {
     public static final String PLAYER_2_NAME = "Player 2";
     public static final String PLAYER_ONE_MARK = "X";
     public static final String PLAYER_TWO_MARK = "O";
+    private static final String RED_BOLD = " \u001b[31;1m";
+    static String BLUE = "\u001b[34m";
+    private static final String RESET = " \u001b[0m";
     static String [] board = {"", "1", "2" ,"3", "4", "5", "6", "7", "8", "9" };
 
     public static void main(String[] args) {
@@ -68,21 +71,34 @@ public class TicTacToe {
 
     private static  void drawBoard() {
         clearScreen();
-        System.out.println();
+        drawBoardHeader();
+        drawBoardBody();
+        drawBoardFooter();
+    }
+
+    private static void drawBoardBody() {
+        System.out.print("  |                      |\n");
+        System.out.print("  |       |     |        |\n\r");
+        System.out.printf("  |    %s  |  %s  |  %s     |\n\r", board[1], board[2], board[3]);
+        System.out.print("  |  -----------------   |\n");
+        System.out.printf("  |    %s  |  %s  |  %s     |\n", board[4], board[5], board[6]);
+        System.out.print("  |  -----------------   |\n");
+        System.out.printf("  |    %s  |  %s  |  %s     |\n", board[7], board[8], board[9]);
+        System.out.print("  |       |     |        |\n");
+        System.out.print("  |                      |");
+    }
+
+    private static void drawBoardFooter() {
+        System.out.println(BLUE);
+        System.out.println("  ========================");
+        System.out.println(RESET);
+    }
+
+    private static void drawBoardHeader() {
+        System.out.println(BLUE);
         System.out.println("  ========================");
         System.out.println("  |   TIC TAC TOE GAME   |");
         System.out.println("  ========================");
-        System.out.println();
-        System.out.print("         |     |\n\r");
-        System.out.printf("      %s  |  %s  |  %s  \n\r", board[1], board[2], board[3]);
-        System.out.print("    -----------------\n");
-        System.out.printf("      %s  |  %s  |  %s  \n", board[4], board[5], board[6]);
-        System.out.print("    -----------------\n");
-        System.out.printf("      %s  |  %s  |  %s  \n", board[7], board[8], board[9]);
-        System.out.print("         |     |\n\r");
-        System.out.println();
-        System.out.println("  ========================");
-        System.out.println();
     }
 
     private static boolean isTie() {
