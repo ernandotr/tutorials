f7c643237cec295d85f3805b3eb0f6d7646b5101
Ernando Rezende <ernando.03@gmail.com>
2025-04-07 00:36:09 +0100

    Created class containig simple ANSI colors

---
+++ src/main/java/br/com/devcompleto/utils/Color.java
@@ -0,0 +1,14 @@
+package br.com.devcompleto.utils;
+
+public class Color {
+
+    public static final String Black = "\u001b[30m";
+    public static final String Red = "\u001b[31m";
+    public static final String Green = "\u001b[32m";
+    public static final String Yellow = "\u001b[33m";
+    public static final String BLUE = "\u001b[34m";
+    public static final String Magenta = "\u001b[35m";
+    public static final String Cyan = "\u001b[36m";
+    public static final String White = "\u001b[37m";
+
+}
