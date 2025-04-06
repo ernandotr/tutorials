b7fee5218a41ed310cc53c93d86fd543c05a188d
Ernando Rezende <ernando.03@gmail.com>
2025-04-07 00:41:09 +0100

    Renamed constants from color names

--- src/main/java/br/com/devcompleto/utils/Color.java
+++ src/main/java/br/com/devcompleto/utils/Color.java
@@ -2,15 +2,15 @@ package br.com.devcompleto.utils;
 
 public class Color {
 
-    private static final String Reset = "\u001b[0m";
+    public static final String RESET = "\u001b[0m";
 
-    public static final String Black = "\u001b[30m";
-    public static final String Red = "\u001b[31m";
-    public static final String Green = "\u001b[32m";
-    public static final String Yellow = "\u001b[33m";
+    public static final String BLACK = "\u001b[30m";
+    public static final String RED = "\u001b[31m";
+    public static final String GREEN = "\u001b[32m";
+    public static final String YELLOW = "\u001b[33m";
     public static final String BLUE = "\u001b[34m";
-    public static final String Magenta = "\u001b[35m";
-    public static final String Cyan = "\u001b[36m";
-    public static final String White = "\u001b[37m";
+    public static final String MAGENTA = "\u001b[35m";
+    public static final String CYAN = "\u001b[36m";
+    public static final String WHITE = "\u001b[37m";
 
 }
