bb5913e98e730c7fb59460efaf7fff400c0c064d
Ernando Rezende <ernando.03@gmail.com>
2025-04-07 23:16:19 +0100

    Added bright colors constants

--- src/main/java/br/com/devcompleto/utils/Color.java
+++ src/main/java/br/com/devcompleto/utils/Color.java
@@ -13,4 +13,13 @@ public class Color {
     public static final String CYAN = "\u001b[36m";
     public static final String WHITE = "\u001b[37m";
 
+    public static final String BRIGHT_BLACK = "\u001b[30;1m";
+    public static final String BRIGHT_RED = "\u001b[31;1m";
+    public static final String BRIGHT_GREEN = "\u001b[32;1m";
+    public static final String BRIGHT_YELLOW = "\u001b[33;1m";
+    public static final String BRIGHT_BLUE = "\u001b[34;1m";
+    public static final String BRIGHT_MAGENTA = "\u001b[35;1m";
+    public static final String BRIGHT_CYAN = "\u001b[36;1m";
+    public static final String BRIGHT_WHITE = "\u001b[37;1m";
+
 }
